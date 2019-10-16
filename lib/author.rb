bab046380e9eeaf03292d8d54061286b33915053
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-16 00:49:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,11 +11,13 @@ class Author
     @@all
   end
   
+  def posts
+  
   def add_post(post)
     post.author = self
   end
 
-  def add_post_by_title(post_title)
+  def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
   end
