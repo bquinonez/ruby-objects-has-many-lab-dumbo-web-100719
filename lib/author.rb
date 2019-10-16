72ef06e8b59ffb2fcd6c9acb36d3aa29ea2fa398
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-16 00:39:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,18 @@
 class Author 
   attr_accessor :name 
   
+  @@all = []
+  
  def initialize(name)
    @name = name
  end
 
   def posts 
-    
+    @posts = posts
   end 
 
+  def add_posts(post)
+    self.Author
+  end
 
 end
